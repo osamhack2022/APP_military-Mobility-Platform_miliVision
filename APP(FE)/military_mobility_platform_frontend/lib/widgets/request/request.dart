7cf41a27402d0f834212788d5743d5acf6694089
CodingVillain <cheolwan.park552@gmail.com>
2022-09-24 07:51:04 +0000

    add: passengers selector

--- APP(FE)/military_mobility_platform_frontend/lib/widgets/request/request.dart
+++ APP(FE)/military_mobility_platform_frontend/lib/widgets/request/request.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:military_mobility_platform_frontend/provider/title.dart';
 import 'package:military_mobility_platform_frontend/widgets/request/location_selector.dart';
+import 'package:military_mobility_platform_frontend/widgets/request/passengers_selector.dart';
 import 'package:provider/provider.dart';
 
 class RequestTab extends StatelessWidget {
@@ -15,9 +16,18 @@ class RequestTab extends StatelessWidget {
     return Column(
       children: [
         const Padding(
-            padding: EdgeInsets.only(top: 50.0), child: LocationSelector()),
+            padding: EdgeInsets.only(top: 40.0), child: LocationSelector()),
         Padding(
-            padding: const EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 20.0),
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+            child: Divider(
+              thickness: 1.0,
+              color: theme.dividerColor,
+            )),
+        const PassengersSelector(),
+        Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
             child: Divider(
               thickness: 1.0,
               color: theme.dividerColor,
