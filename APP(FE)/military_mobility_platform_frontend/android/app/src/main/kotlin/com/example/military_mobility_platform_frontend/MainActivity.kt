e90f48117107716b36dfa6cac25835bd1fe6bedb
Cheolwan-Park <cheolwan.park552@gmail.com>
2022-09-24 00:33:33 +0000

    initial commit

---
+++ APP(FE)/military_mobility_platform_frontend/android/app/src/main/kotlin/com/example/military_mobility_platform_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.military_mobility_platform_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ APP(FE)/military_mobility_platform_frontend/lib/widgets/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Military Mobility Platform')),
+      body: const Center(
+        child: Text("Home"),
+      ),
+    );
+  }
+}
