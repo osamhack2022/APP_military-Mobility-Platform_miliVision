399eaf25e05b29ef649b934e466aa727b6b9b779
jkworldchampion <standardjuhwan@gmail.com>
2022-10-01 05:11:02 +0000

    add Driver class in model

--- APP(BE)/web-server/backend/db_schema/models.py
+++ APP(BE)/web-server/backend/db_schema/models.py
@@ -14,3 +14,13 @@ class Car(models.Model):
     need_license = models.IntegerField()        # 면허의 포함관계를 설정해 수정 필요
     
     
+class Driver(models.Model):
+    driver_id = models.AutoField(primary_key=True)  
+    name = models.CharField(max_length=15)        # 홍길동
+    rank = models.IntegerField()                  # 0=이등병, 13=대장
+    department = models.CharField(max_length=100) # ~~대대~~중대~~소대
+    mileage = models.IntegerField()               
+    license = models.IntegerField()
+    
+    
+    
