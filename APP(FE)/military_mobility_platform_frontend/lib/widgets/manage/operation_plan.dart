1df46440c5cde241a8c955b6611828a4a4f51cec
Seung Hyo Lee <52388644+AstroHyo@users.noreply.github.com>
2022-10-03 14:28:01 +0000

    add: manage tab set navigator

--- APP(FE)/military_mobility_platform_frontend/lib/widgets/manage/operation_plan.dart
+++ APP(FE)/military_mobility_platform_frontend/lib/widgets/manage/operation_plan.dart
@@ -5,8 +5,9 @@ class OperationPlan extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const subtitle = '운전 계획 작성';
-    return InkWell(
+    const subtitle = '운행 계획 작성';
+    return Material( child:
+    GestureDetector(
       child: 
         Container(
           width: 168,
@@ -27,15 +28,62 @@ class OperationPlan extends StatelessWidget {
             children: [
               Container(
                 margin: EdgeInsets.fromLTRB(0,25,0,20),
-                child: Icon(Icons.menu_book_outlined, size: 60),
+                child: Icon(Icons.checklist, size: 60),
               ),
               Text(subtitle, style: TextStyle(fontSize: 16)),
             ]
           )
         ),
         onTap: () {
-          
+          Navigator.push(
+            context, MaterialPageRoute(builder: (context) => OperationPlanSet())
+          );
         },
+    )
+    );
+  }
+}
+
+class OperationPlanSet extends StatefulWidget {
+  const OperationPlanSet({super.key});
+
+  @override
+  State<OperationPlanSet> createState() => _OperationPlanSetState();
+}
+
+class _OperationPlanSetState extends State<OperationPlanSet> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            color: Colors.black,
+            iconSize: 15.0,
+            onPressed: () {Navigator.of(context).pop();},
+          ),
+          const Padding(
+              padding: EdgeInsets.only(bottom: 10.0)
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Text('운행 계획 작성', style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.bold)),
+          ),
+          const Padding(
+              padding: EdgeInsets.only(bottom: 10.0)
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(
+              onPressed: () => print('clicked'), 
+              child: const Text('운행 계획 작성하기', style: TextStyle(fontSize: 18.0)),
+            ),
+          ),
+        ],
+      )
     );
   }
 }
