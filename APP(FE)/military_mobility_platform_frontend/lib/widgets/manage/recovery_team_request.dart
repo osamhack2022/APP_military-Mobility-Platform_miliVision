7e343fe5766c37f4e654af542f66ad86a114b326
Seung Hyo Lee <52388644+AstroHyo@users.noreply.github.com>
2022-10-03 14:55:41 +0000

    fix: manage tab navigator widget name

--- APP(FE)/military_mobility_platform_frontend/lib/widgets/manage/recovery_team_request.dart
+++ APP(FE)/military_mobility_platform_frontend/lib/widgets/manage/recovery_team_request.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class EmergencyEvacuationRequest extends StatelessWidget {
-  const EmergencyEvacuationRequest({super.key});
+class RecoveryTeamRequest extends StatelessWidget {
+  const RecoveryTeamRequest({super.key});
 
   @override
   Widget build(BuildContext context) {
