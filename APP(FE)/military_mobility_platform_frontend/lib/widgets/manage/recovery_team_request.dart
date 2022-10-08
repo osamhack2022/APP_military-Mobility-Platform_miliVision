6616120ab0d0194c3227c7209e28dce9e6768bda
Seung Hyo Lee <52388644+AstroHyo@users.noreply.github.com>
2022-10-08 13:46:23 +0000

    install map packages

--- APP(FE)/military_mobility_platform_frontend/lib/widgets/manage/recovery_team_request.dart
+++ APP(FE)/military_mobility_platform_frontend/lib/widgets/manage/recovery_team_request.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:map_pin_picker/map_pin_picker.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class RecoveryTeamRequest extends StatelessWidget {
   const RecoveryTeamRequest({super.key});
@@ -52,13 +57,22 @@ class VehicleLocationCheck extends StatefulWidget {
 }
 
 class _VehicleLocationCheckState extends State<VehicleLocationCheck> {
- 
+  final _controller = Completer<GoogleMapController>();
+  MapPickerController mapPickerController = MapPickerController();
+
+  CameraPosition cameraPosition = const CameraPosition(
+    target: LatLng(41.311158, 69.279737),
+    zoom: 14.4746,
+  );
+
+  var textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+        children: [/*
           IconButton(
             icon: Icon(Icons.arrow_back_ios),
             color: Colors.black,
@@ -74,14 +88,102 @@ class _VehicleLocationCheckState extends State<VehicleLocationCheck> {
           ),
           const Padding(
               padding: EdgeInsets.only(bottom: 10.0)
+          ),*/
+          Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              MapPicker(
+                // pass icon widget
+                /*iconWidget: SvgPicture.asset(
+                  "assets/location_icon.svg",
+                  height: 60,
+                ),*/
+                //add map picker controller
+                mapPickerController: mapPickerController,
+                child: GoogleMap(
+                  myLocationEnabled: true,
+                  zoomControlsEnabled: false,
+                  // hide location button
+                  myLocationButtonEnabled: false,
+                  mapType: MapType.normal,
+                  //  camera position
+                  initialCameraPosition: cameraPosition,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                  onCameraMoveStarted: () {
+                    // notify map is moving
+                    mapPickerController.mapMoving!();
+                    textController.text = "checking ...";
+                  },
+                  onCameraMove: (cameraPosition) {
+                    this.cameraPosition = cameraPosition;
+                  },
+                  onCameraIdle: () async {
+                    // notify map stopped moving
+                    mapPickerController.mapFinishedMoving!();
+                    //get address name from camera position
+                    List<Placemark> placemarks = await placemarkFromCoordinates(
+                      cameraPosition.target.latitude,
+                      cameraPosition.target.longitude,
+                    );
+
+                    // update the ui with the address
+                    textController.text =
+                    '${placemarks.first.name}, ${placemarks.first.administrativeArea}, ${placemarks.first.country}';
+                  },
+                ),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).viewPadding.top + 20,
+                width: MediaQuery.of(context).size.width - 50,
+                height: 50,
+                child: TextFormField(
+                  maxLines: 3,
+                  textAlign: TextAlign.center,
+                  readOnly: true,
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.zero, border: InputBorder.none),
+                  controller: textController,
+                ),
+              ),
+              /*Positioned(
+                bottom: 24,
+                left: 24,
+                right: 24,
+                child: SizedBox(
+                  height: 50,
+                  child: TextButton(
+                    child: const Text(
+                      "Submit",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        color: Color(0xFFFFFFFF),
+                        fontSize: 19,
+                        // height: 19/19,
+                      ),
+                    ),
+                    onPressed: () {
+                      print(
+                          "Location ${cameraPosition.target.latitude} ${cameraPosition.target.longitude}");
+                      print("Address: ${textController.text}");
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                      MaterialStateProperty.all<Color>(const Color(0xFFA3080C)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )*/
+            ],
           ),
-          //지도 api 대체
-          Image.network(
-            'https://user-images.githubusercontent.com/2384779/52900496-bb651780-3239-11e9-8a01-27f4cca590cb.jpg',
-            width: 700.0,
-            height: 350.0,
-            fit: BoxFit.cover,
-          ),
+          /*
           Padding(
             padding: const EdgeInsets.all(10),
             child: ElevatedButton(
@@ -92,7 +194,7 @@ class _VehicleLocationCheckState extends State<VehicleLocationCheck> {
               },
               child: const Text('다음', style: TextStyle(fontSize: 18.0)),
             ),
-          ),
+          ),*/
         ],
       )
     );
