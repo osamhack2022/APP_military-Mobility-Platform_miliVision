522d976a050d42dc44ee8305de3ded01097a2ad8
CodingVillain <cheolwan.park552@gmail.com>
2022-10-10 03:45:13 +0000

    add: kepler.gl

--- APP(WEB)/geodata-visualizer/src/index.js
+++ APP(WEB)/geodata-visualizer/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import document from 'global/document';
+import { Provider } from 'react-redux';
+import { render } from 'react-dom';
+import store from './store';
+import App from './app';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const Root = () => (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<Root />, document.body.appendChild(document.createElement('div')));
