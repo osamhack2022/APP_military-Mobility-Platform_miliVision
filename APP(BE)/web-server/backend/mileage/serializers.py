59ae66d781d895657db8d512f250a407a5163b2e
jkworldchampion <standardjuhwan@gmail.com>
2022-10-16 05:06:08 +0000

    update history post api

--- APP(BE)/web-server/backend/mileage/serializers.py
+++ APP(BE)/web-server/backend/mileage/serializers.py
@@ -5,4 +5,9 @@ class HistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = History
         fields = '__all__'
+        
+class AddHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = History
+        fields = ('car_id', 'department', 'arrival', 'datetime', 'total_time', 'total_range')
     
--- APP(BE)/web-server/backend/mileage/views.py
+++ APP(BE)/web-server/backend/mileage/views.py
@@ -24,30 +24,18 @@ class history(APIView):
                                                '''
                                                )
     def get(self, request):
-        # try:
-        #     token = request.META['HTTP_AUTHORIZATION'][7:]
-        #     user = get_user_from_access_token(token)
-        #     user_id = user.id
-        #     # user_id = request.GET['user_id']
-        #     # return Response(get_history_by_user(user_id), status=status.HTTP_200_OK)
-        #     return Response(get_history(), status=status.HTTP_200_OK)
-            
-        # except Exception as e:
-        #     print(e)
-        #     return Response(status=status.HTTP_400_BAD_REQUEST)
         token = request.META['HTTP_AUTHORIZATION'][7:]
         user = get_user_from_access_token(token)
         historys = History.objects.filter(user_id=user.id)
         serializer = HistorySerializer(historys, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK) 
     
-    @swagger_auto_schema(request_body=HistorySerializer, 
+    @swagger_auto_schema(request_body=AddHistorySerializer, 
                          operation_summary="주행기록 작성하기",
                          operation_description='''
                                                 ---request---
-                                                user_id: (int)요청한 사용자의 id / ex) 4
                                                 car_id: (int)탑승한 차의 id / ex) 1111001
-                                                department: (string) 출발지 / ex) 서울
+                                                departure: (string) 출발지 / ex) 서울
                                                 arrival: (string) 도착지 / ex) 진해
                                                 datetime: (datetime) 운행한 날짜 / ex) 2022-10-10T13:54:39.700000+09:00
                                                 total_time: (int) 운행한 총 시간 / ex) 7
@@ -57,9 +45,11 @@ class history(APIView):
                                                 model: History
                                                ''')
     def post(self, request):
-        serializer = HistorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+        serializer = AddHistorySerializer(data=request.data)  # user_id를 빼고 입력 받는 serializer
+        if serializer.is_valid():  # serializer가 유효 하다면
+            token = request.META['HTTP_AUTHORIZATION'][7:]
+            user_id = get_user_from_access_token(token)
+            serializer.save(user_id = user_id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
